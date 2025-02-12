dc63890f59b58d83201e9dee36065c0cfd0c7a94
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2025-02-12 23:24:15 +0600

    I have created my app for form and also added image

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import silver from './assets/silver.jpg';
+
+export const App = () => (
+    <div>
+
+        <div className='main-block'>
+            <div className='block-image'>
+                <img className='image-silver' src={silver} alt="silver"/>
+            </div>
+
+            <div className='block-1'>
+
+            </div>
+        </div>
+    </div>
+);
